4b4af478ce8de24f4a2ec04cc84e0a5b80b7863c
balarsen <balarsen>
2010-05-24 22:22:25 +0000

    examples for config file reading and writing, use in other places as needd

---
+++ spacepy/sandbox/readconfig.py
@@ -0,0 +1,26 @@
+## this is an example configuration reading tool
+## MOdify and add to whatever as needed
+## Brian Larsen 24-May-2010
+
+import ConfigParser
+def readconfig(my_cfg, config_filepath):
+    # Create a ConfigParser object, to read the config file
+    cfg=ConfigParser.ConfigParser()
+    fp=open(config_filepath, "rb")
+    cfg.readfp(fp)
+    fp.close()
+    # Read each parameter in turn
+    # Web section values
+    if cfg.has_option("web", "port"):
+        my_cfg["PORT"]=cfg.get("web", "port")
+    if cfg.has_option("web", "browser_refresh"):
+        my_cfg["BROWSER_REFRESH"]=cfg.get("web", "browser_refresh")
+    # service section values
+    if cfg.has_option("service", "service_name"):
+        my_cfg["SERVICE_NAME"]=cfg.get("service", "service_name")
+    if cfg.has_option("service", "poll_interval"):
+        my_cfg["POLL_INTERVAL"]=cfg.get("service", "poll_interval")
+    if cfg.has_option("service", "log_records"):
+        my_cfg["LOG_RECORDS"]=cfg.get("service", "log_records")
+    return
+                    
---
+++ spacepy/sandbox/writeconfig.py
@@ -0,0 +1,18 @@
+## this is an example configuration writing tool
+## MOdify and add to whatever as needed
+## Brian Larsen 24-May-2010
+
+import ConfigParser
+def writeconfig(my_cfg, config_filepath):
+    cfg=ConfigParser.ConfigParser()
+    cfg.add_section("web")
+    cfg.add_section("service")
+    cfg.set("web", "port", my_cfg["PORT"])
+    cfg.set("web", "browser_refresh", my_cfg["BROWSER_REFRESH"])
+    cfg.set("service", "service_name", my_cfg["SERVICE_NAME"])
+    cfg.set("service", "poll_interval", my_cfg["POLL_INTERVAL"])
+    cfg.set("service", "log_records", my_cfg["LOG_RECORDS"])
+    fp=open(config_filepath, "wb")
+    cfg.write(fp)
+    fp.close()
+    return
