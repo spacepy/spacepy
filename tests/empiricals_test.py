45e17e9e3a3a94b707bef5704e9751016ca60a4d
balarsen <balarsen>
2010-09-03 20:44:56 +0000

    skeleton test file

---
+++ tests/empiricals_test.py
@@ -0,0 +1,30 @@
+
+import unittest
+import spacepy.empiricals as emp
+
+        
+
+class tFunctionTests(unittest.TestCase):
+    def setUp(self):
+        #super(tFunctionTests, self).setUp()
+        pass
+
+    def tearDown(self):
+        #super(tFunctionTests, self).tearDown()
+        pass
+    
+
+if __name__ == "__main__":
+    ## suite = unittest.TestLoader().loadTestsFromTestCase(SimpleFunctionTests)
+    ## unittest.TextTestRunner(verbosity=2).run(suite)
+
+    ## suite = unittest.TestLoader().loadTestsFromTestCase(tFunctionTests)
+    ## unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+    unittest.main()
+
+
+
+
+
