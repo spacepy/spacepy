a26c9027bd5ceb582c43f13e784ecef10f38d080
balarsen <balarsen>
2010-09-03 20:09:12 +0000

    moved tests into their own directory

--- tests/toolbox_test.py
+++ tests/toolbox_test.py
@@ -135,7 +135,11 @@ class tFunctionTests(unittest.TestCase):
 
 
 if __name__ == "__main__":
-	unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleFunctionTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+#	unittest.main()
 
 
 
