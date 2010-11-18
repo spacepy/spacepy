24420fab8d67a96cdf424ca4d8c2d6559c6a271d
jkoller <jkoller>
2010-11-18 19:40:41 +0000

    moved some files around and added a Quickstartguide.pdf in the top directory

--- QuickStart/conf.py
+++ QuickStart/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'QuickStartGuide'
-copyright = u'2010, Josef Koller'
+copyright = u'2010, The SpacePy Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
