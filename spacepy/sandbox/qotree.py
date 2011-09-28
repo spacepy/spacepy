3a7baad738186cb16642b4e0d71cd5fd7480c624
Brian Larsen <balarsen@lanl.gov>
2011-09-28 12:05:02 -0600

    far from complete testing but a good step along the way.  First generalized QTree created and runs

--- spacepy/sandbox/qotree.py
+++ spacepy/sandbox/qotree.py
@@ -9,14 +9,52 @@ Origional by Dan Welling, cleanup and subclassing by Brian Larsen
 
 import numpy as np
 
+def pybats_decision(tree=None, branchNum=None):
+    a = np.sqrt(tree[branchNum].npts)
+    if (int(a) == a) and (a > 0 ):
+        dx = (np.max(grid[0,:][tree[branchNum].locs]) \
+                  - np.min(grid[0,:][tree[branchNum].locs])) \
+                  / (np.sqrt(tree[branchNum].npts)-1)
+        if not np.isnan(dx):
+            if int(np.log2(dx))==np.log2(dx):
+                # An NxN block can be considered a "leaf" or stopping point
+                # if Dx is an integer power of 2.  Leafs must "know" the
+                # indices of the x,y points located inside of them as a
+                # grid and also know the bounding coords of the grid cells.
+                #tree[branchNum].isLeaf = True
+                tree[branchNum].locs=self[i].locs.reshape( (a, a) )
+                tree[branchNum].dx = dx
+                tree[branchNum].cells = np.meshgrid(
+                    np.arange(tree[branchNum].lim[0], tree[branchNum].lim[1]+dx, dx),
+                    np.arange(tree[branchNum].lim[2], tree[branchNum].lim[3]+dx, dx))
+                return False
+        else:
+            return False
+    else:
+        return True
 
-class QTree(object):
+
+
+def leftdaughter(dim, k):
+    return k*2**dim-2**dim+2
+
+def rightdaughter(dim, k):
+    return k*2**dim+1
+
+def mother(dim, k):
+    return (k+2**dim-2)/2**dim
+
+def boxes_through_level(dim, level):
+    return ((2**dim)**level - 1)/(2**dim - 1)
+
+
+class QTree(dict):
     '''
     Base class for Quad/Oct tree objects assuming cell-centered grid points
     in a rectangular non-regular layout.
     '''
 
-    def __init__(self, grid):
+    def __init__(self, grid, grid_edges=False, max_depth = 3, decision_func = None):
         '''
         Build QO Tree for input grid.  Grid should be a NxM numpy array where
         N is the number of dimensions and M is the number of points.
@@ -26,96 +64,94 @@ class QTree(object):
         grid, numpy.ndarray
             Grid should be a NxM numpy array where N is the number of dimensions and M is the number of points.
         '''
+        if (grid.shape[0] != 2):
+            raise(NotImplementedError("Sorry, QTrees are for 2D grids only."))
+
+        self.decision_func = decision_func
+
+        self.max_depth = max_depth
+        self.grid = grid
+
         (self.d, self.npoints) = grid.shape
 
-        if(self.d != 2):
-            raise NotImplementedError, "Sorry, QTrees are for 2D grids only."
         self.tree = {}
 
-        # Find limits of cell-centered grid.
-        # Get values and locations of grid max/min
-        minloc, xmin = grid[0,:].argmin(), grid[0,:].min()
-        maxloc, xmax = grid[0,:].argmax(), grid[0,:].max()
-        # Distance from min X value is grid size at that point.
-        r = np.sqrt((grid[0,:]-xmin)**2. + (grid[1,:]-grid[1,:][minloc])**2.)
-        ml=(np.where(r>0, r, 10000)).min()
-        # Actual min is not cell center, but cell boundary.
-        xmin-=ml/2.
-        # Repeat for Xmax.
-        r = np.sqrt((grid[0,:]-xmax)**2. +
-                 (grid[1,:]-grid[1,:][maxloc])**2.)
-        ml=(np.where(r>0, r, 10000)).min()
-        xmax+=ml/2.
+        if not grid_edges:
+            #####
+            # toolbox.bin_center_to_edges can be used to make centers into edges
+            #####
+            # Find limits of cell-centered grid.
+            # Get values and locations of grid max/min
+            minloc, xmin = grid[0,:].argmin(), grid[0,:].min()
+            maxloc, xmax = grid[0,:].argmax(), grid[0,:].max()
+            # Distance from min X value is grid size at that point.
+            r = np.sqrt((grid[0,:]-xmin)**2. + (grid[1,:]-grid[1,:][minloc])**2.)
+            ml=r[r>0].min()
+            # Actual min is not cell center, but cell boundary.
+            xmin -= ml/2.
+            # Repeat for Xmax.
+            r = np.sqrt((grid[0,:]-xmax)**2. + (grid[1,:]-grid[1,:][maxloc])**2.)
+            ml=r[r>0].min()
+            xmax += ml/2.
 
-        # Repeat for Ymin/max.
-        minloc, ymin = grid[1,:].argmin(),grid[1,:].min()
-        maxloc, ymax = grid[1,:].argmax(),grid[1,:].max()
+            # Repeat for Ymin/max.
+            minloc, ymin = grid[1,:].argmin(), grid[1,:].min()
+            maxloc, ymax = grid[1,:].argmax(), grid[1,:].max()
 
-        r = np.sqrt((grid[1,:]-ymin)**2. + (grid[0,:]-grid[0,:][minloc])**2.)
-        ml=(np.where(r>0, r, 10000)).min(); ymin-=ml/2.
-        r = np.sqrt((grid[1,:]-ymax)**2. + (grid[0,:]-grid[0,:][maxloc])**2.)
-        ml=(np.where(r>0, r, 10000)).min(); ymax+=ml/2.
+            r = np.sqrt((grid[1,:]-ymin)**2. + (grid[0,:]-grid[0,:][minloc])**2.)
+            ml=r[r>0].min()
+            ymin -= ml/2.
+            r = np.sqrt((grid[1,:]-ymax)**2. + (grid[0,:]-grid[0,:][maxloc])**2.)
+            ml=r[r>0].min()
+            ymax += ml/2.
 
-        self.aspect_ratio = (xmax-xmin)/(ymax-ymin)
+            self.aspect_ratio = (xmax-xmin)/(ymax-ymin)
 
-        # Use spatial range of grid to seed root of QTree.
-        self[1]=[xmin,xmax,ymin,ymax]
-        self.locs = np.lexsort( (grid[1,:], grid[0,:]) ) # lexsort works from the right
-        self._spawn_daughters(grid)
+            # Use spatial range of grid to seed root of QTree.
+            self[1] = Branch(np.asarray([xmin,xmax,ymin,ymax]))
+            self.locs = np.lexsort( (grid[1,:], grid[0,:]) ) # lexsort works from the right
+            self._spawn_daughters()
+        else:
+            raise(NotImplementedError("Sorry, Only allowed to use grid centers so far"))
 
-    def _spawn_daughters(self, grid, i=1):
+    def _spawn_daughters(self, i=1):
         '''
         Internal recursive method for populating tree.
         '''
-        self[i].locs=self.locs[(grid[0,:][self.locs]>self[i].lim[0]) &
-                               (grid[0,:][self.locs]<self[i].lim[1]) &
-                               (grid[1,:][self.locs]>self[i].lim[2]) &
-                               (grid[1,:][self.locs]<self[i].lim[3]) ]
+        self[i].locs = self.locs[(self.grid[0,:][self.locs]>self[i].lim[0]) &
+                               (self.grid[0,:][self.locs]<self[i].lim[1]) &
+                               (self.grid[1,:][self.locs]>self[i].lim[2]) &
+                               (self.grid[1,:][self.locs]<self[i].lim[3]) ]
         self[i].npts = self[i].locs.size
-        a = np.sqrt(self[i].npts)
-        if (int(a) == a) :
-            dx = (np.max(grid[0,:][self[i].locs]) \
-                      - np.min(grid[0,:][self[i].locs])) \
-                      / (np.sqrt(self[i].npts)-1)
-            if int(np.log2(dx))==np.log2(dx):
-                # An NxN block can be considered a "leaf" or stopping point
-                # if Dx is an integer power of 2.  Leafs must "know" the
-                # indices of the x,y points located inside of them as a
-                # grid and also know the bounding coords of the grid cells.
+        if self.decision_func != None:
+            if not self.decision_func(self, i):
                 self[i].isLeaf = True
-                self[i].locs=self[i].locs.reshape( (a, a) )
-                self[i].dx = dx
-                self[i].cells = np.meshgrid(
-                    np.arange(self[i].lim[0], self[i].lim[1]+dx, dx),
-                    np.arange(self[i].lim[2], self[i].lim[3]+dx, dx))
                 return
-
         # Subdivide section into four new ones (8 if oct tree)
         dx = (self[i].lim[1] - self[i].lim[0])/2.0
         x=[self[i].lim[0], self[i].lim[0]+dx, self[i].lim[0]+dx, self[i].lim[0]]
         y=[self[i].lim[2], self[i].lim[2], self[i].lim[2]+dx, self[i].lim[2]+dx]
-        for j, k in enumerate(range(self.ld(i), self.rd(i)+1)):
-            self[k] = [x[j],x[j]+dx,y[j],y[j]+dx]
-            self._spawn_daughters(grid,k)
+        for j, k in enumerate(range(self.getleftdaughter(i), self.getrightdaughter(i)+1)):
+            self[k] = Branch(np.asarray([x[j],x[j]+dx,y[j],y[j]+dx]))
+            # if we are at max depth we don't want to split again
+            # this is tested by seeing if k is in the lowest level
+            if k <= self.getboxes_through_level(self.max_depth-1):
+                self._spawn_daughters(k)
+            else:
+                self[k].isLeaf = True
 
 
-    def __getitem__(self, key):
-        return self.tree[key]
+    def getboxes_through_level(self, level):
+        return boxes_through_level(self.d, level)
 
-    def __setitem__(self, key, value):
-        self.tree[key] = Branch(value)
+    def getmother(self, k):
+        return mother(self.d, k)
 
-    def keys(self):
-        return self.tree.keys()
+    def getleftdaughter(self, k):
+        return leftdaughter(self.d, k)
 
-    def mom(self, k):
-        return (k+2**self.d-2)/2**self.d
-    def leftdaughter(self, k):
-        return k*2**self.d-2**self.d+2
-    def rightdaughter(self,k):
-        return k*2**self.d+1
-    rd = rightdaughter
-    ld = leftdaughter
+    def getrightdaughter(self, k):
+        return rightdaughter(self.d, k)
 
     def plot_res(self, ax, DoLabel=True):
         res_colors={
@@ -158,6 +194,10 @@ class Branch(object):
         lim should be a 4 element list of the
         dimensional boundaries of the branch.
         '''
+        try:
+            assert(len(lim) == 4)
+        except AssertionError:
+            raise(ValueError("Limits can only be a 4 element array"))
         self.isLeaf = False
         self.lim = lim
 
--- spacepy/sandbox/test_qotree.py
+++ spacepy/sandbox/test_qotree.py
@@ -13,6 +13,24 @@ import numpy
 import qotree
 
 
+class staticTests(unittest.TestCase):
+    def setUp(self):
+        super(staticTests, self).setUp()
+        self.x = numpy.linspace(1, 100, 150)
+        self.y = numpy.linspace(1, 100, 150)
+        self.grid = numpy.vstack((self.x, self.y))
+
+    def tearDown(self):
+        super(staticTests, self).tearDown()
+
+
+    def test_leftdaughter(self):
+        """leftdaughter shold give known output"""
+        ans = [2, 102, 202, 302, 402, 502, 602, 702]
+        for i, ii in enumerate(range(1, 200, 25)):
+            self.assertEqual(qotree.leftdaughter(2, ii), ans[i])
+
+
 class qotreeTests(unittest.TestCase):
     def setUp(self):
         super(qotreeTests, self).setUp()
@@ -23,13 +41,19 @@ class qotreeTests(unittest.TestCase):
     def tearDown(self):
         super(qotreeTests, self).tearDown()
 
+
     def test_QTree_imputs(self):
         """QTree does some input checking"""
         self.assertRaises(NotImplementedError, qotree.QTree, numpy.vstack((self.grid, self.x)))
 
     def test_creation(self):
         """create a QTree and test its contents (regression)"""
-        qt = qotree.QTree(self.grid)
+        qt = qotree.QTree(self.grid, max_depth=2)
+        self.assertEqual(qt.d, 2)
+        self.assertEqual(qt.keys(), [1, 2, 3, 4, 5])
+        self.assertEqual(qt.getleftdaughter(1), 2)
+        self.assertEqual(qt.getrightdaughter(1), 5)
+        
 
 
 if __name__ == "__main__":
