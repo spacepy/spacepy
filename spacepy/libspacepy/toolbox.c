ff935798ba0fc1055012d6e149047fce7767bd8c
Brian Larsen <balarsen@lanl.gov>
2011-10-31 13:58:53 -0600

    toolbox: format fix

--- spacepy/libspacepy/toolbox.c
+++ spacepy/libspacepy/toolbox.c
@@ -3,16 +3,15 @@
 Copyright ©2011 Los Alamos National Security, LLC.
 */
 
+#include <math.h>
 
-/********
+/*******************************************************************************
  * Given an array of doubles compute the hypot math.sqrt(sum((v ** 2 for v in vals[0])))
  * Brian Larsen
  * 31 Oct 2011
-********/
-
-#include <math.h>
-
+*******************************************************************************/
 double hypot_tb(double *data, long len) {
+    
     double sm=0.0;
     long i;
     for (i=0;i<len;i++) {
