5ce6bf655249f31e6f9e0e82e4821d2541bc5409
Jonathan Niehof <jniehof@lanl.gov>
2012-01-24 13:25:17 -0700

    pycdf docs: module-level fixup, specifically library search.

--- spacepy/pycdf/__init__.py
+++ spacepy/pycdf/__init__.py
@@ -7,37 +7,44 @@ library used for many NASA missions, available at http://cdf.gsfc.nasa.gov/.
 It is targeted at Python 2.6+ and should work without change on either
 Python 2 or Python 3.
 
-The interface is intended to be quite 'pythonic' rather than reproducing the
-C interface. To open or close a CDF and access its variables, see the :py:class:`pycdf.CDF`
-class documentation. Accessing data within the variables is via the :py:class:`pycdf.Var`
-class. The :py:class:`pycdf.lib` object (of type :py:class:`pycdf.lib`) provides access to some routines
-that affect the functionality of the library in general.
+The interface is intended to be 'pythonic' rather than reproducing the
+C interface. To open or close a CDF and access its variables, see the :class:`CDF`
+class. Accessing data within the variables is via the :class:`Var`
+class. The :data:`lib` object provides access to some routines
+that affect the functionality of the library in general. The
+:mod:`~spacepy.pycdf.const` module contains constants useful for accessing
+the underlying library.
 
-The base CDF library must be properly installed in order to use this package.
-pycdf will search for the library in this order:
 
-1. A directory named by the environment variable CDF_LIB (which should be set if using the definitions file provided with the CDF library).
-2. A subdirectory C{lib} in a directory named by the environment variable CDF_BASE.
-3. The standard system library search path.
+The CDF C library must be properly installed in order to use this package.
+The CDF distribution provides scripts meant to be called in a user's
+login scripts, ``definitions.B`` for bash and ``definitions.C`` for C-shell
+derivatives. (See the installation instructions which come with the CDF library.)
+These will set environment variables specifying the location
+of the library; pycdf will respect these variables if they are set. Otherwise
+it will search the standard system library path and the default installation
+locations for the CDF library.
 
-If pycdf has trouble finding the library, try setting CDF_LIB before importing
-the module, e.g. if the library is in CDF/lib in the user's home directory::
-  import os
-  os.putenv("CDF_LIB", "~/CDF/lib")
-  import pycdf
+If pycdf has trouble finding the library, try setting ``CDF_LIB`` before importing
+the module, e.g. if the library is in ``CDF/lib`` in the user's home directory:
+    >>> import os
+    >>> os.putenv("CDF_LIB", "~/CDF/lib")
+    >>> import pycdf
+If this works, make the environment setting permanent. Note that on OSX,
+using plists to set the environment may not carry over to Python terminal
+sessions; use ``.cshrc`` or ``.bashrc`` instead.
 
 .. currentmodule:: spacepy.pycdf
 
-The :py:mod:`pycdf.const` module contains constants useful for accessing the underlying library.
-
-.. NOTE... there is an error with this reference
 
 Authors: Jon Niehof
+
 Institution: Los Alamos National Laboratory
+
 Contact: jniehof@lanl.gov
 
 
-Copyright 2010 Los Alamos National Security, LLC.
+Copyright 2010-2012 Los Alamos National Security, LLC.
 
 """
 
