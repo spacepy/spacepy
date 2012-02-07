e4bbc68d686e26d6277496ccdb91937cdf7ce7ae
Brian Larsen <balarsen@lanl.gov>
2012-02-07 16:23:31 -0700

    benchmark: moment in time benchmark of linspace toolbox vs numpy

--- spacepy/sandbox/benchmark/linspace.py
+++ spacepy/sandbox/benchmark/linspace.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt 
 import timeit
+import os
 
 import numpy as np
 import spacepy.toolbox as tb
@@ -10,19 +11,34 @@ import spacepy.toolbox as tb
 import dulwich
 
 ans = {}
-for val in range(1, 9):
+for val in xrange(1, 9):
     tb_t = timeit.Timer("tb.linspace(1.5, 10.5, " + str(10**(val/2)) + ")", setup="import spacepy.toolbox as tb")
     np_t = timeit.Timer("np.linspace(1.5, 10.5, " + str(10**(val/2)) + ")", setup="import numpy as np")
-    val = [np_t.timeit(10000)/tb_t.timeit(10000) for v in range(50)]    
-    ans[10**(val/2)] = [np.mean(val), np.std(val)]
+    bnh = [np_t.timeit(10000)/tb_t.timeit(10000) for v in range(50)]    
+    ans[10**(val/2)] = [np.mean(bnh), np.std(bnh)]
+    tb.progressbar(val, 1, len(xrange(1, 9)), 'Progress')
     
 x = np.array(ans.keys())
 x.sort()
 y = np.asarray([ans[v][0] for v in x])
 yerr = np.asarray([ans[v][1] for v in x])
-plt.errorbar(x, y, yerr, color='b')
+plt.errorbar(x, y, yerr, color='b', fmt='o-')
 plt.xscale('log')
-plt.xtitle('linspace points')
-plt.ytitle('tb.linspace/np.linspace')
+plt.xlabel('linspace points')
+plt.ylabel('tb.linspace/np.linspace')
 # get the title
+rep = dulwich.repo.Repo(os.path.abspath('../../../'))
+refs = rep.get_refs()
+title = refs['HEAD']
+plt.title(title)
+plt.savefig('linspace_bench.png')
+
+
+## CONCLUSION:
+#   the toolbox version of linspace is faster than the numpy version for all tested range sizes
+#   when the size is small it is ~16x an when it is large ~2x
+
+
+
+
 
