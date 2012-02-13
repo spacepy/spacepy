46abe5ec057b9b438e86130852a762592f64a110
Brian Larsen <balarsen@lanl.gov>
2012-02-13 15:08:36 -0700

    benchmark: rewritten with the much easier benchmarking suite Benchmarker    * see: http://pypi.python.org/pypi/Benchmarker/

--- developer/benchmarks/hypot.py
+++ developer/benchmarks/hypot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import timeit
 import os
 import ctypes
+import math
 
 import numpy as np
 import spacepy.toolbox as tb
@@ -17,93 +18,37 @@ lib = ctypes.CDLL('hypot.so')
 lib.hypot_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_long]
 lib.hypot.restype = ctypes.c_double
 
+
+from benchmarker import Benchmarker
 #==============================================================================
-# Compare ctypes and pyhton
+# Compare them all
 #==============================================================================
+lib = ctypes.CDLL('hypot.so')
+lib.hypot_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_long]
+lib.hypot.restype = ctypes.c_double
 
-ans = {}
-for ii, val in enumerate(tb.logspace(3, 1e5, 10)):
-    ctypes_t = timeit.Timer("lib.hypot(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))", 
-                            setup="import ctypes; lib = ctypes.CDLL('hypot.so'); " +
-                            "lib.hypot_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_long];" +
-                            "lib.hypot.restype = ctypes.c_double; " + 
-                            "import numpy as np;" + 
-                            "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    python_t = timeit.Timer("math.sqrt(sum([v**2 for v in data]))", 
-                           setup="import numpy as np; import ctypes; import math;" + 
-                           "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    bnh = [ctypes_t.timeit(100)/python_t.timeit(100) for v in range(1)]    
-    ans[int(val)] = [np.mean(bnh), np.std(bnh)]
-    tb.progressbar(ii, 1, len(tb.logspace(3, 1e5, 10)), 'Progress')
-    
-print()
-x = np.array(ans.keys())
-x.sort()
-y = np.asarray([ans[v][0] for v in x])
-yerr = np.asarray([ans[v][1] for v in x])
-plt.errorbar(x, y, yerr, color='b', fmt='o-', label='ctypes/python')
+def ctypes_t(n):
+    data = np.arange(1, n , dtype=ctypes.c_double)
+    ans = lib.hypot(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
 
-#==============================================================================
-# Compare numpy an python
-#==============================================================================
+def python_t(n):
+    data = np.arange(1, n , dtype=ctypes.c_double)
+    ans = math.sqrt(sum([v**2 for v in data]))
 
-ans = {}
-for ii, val in enumerate(tb.logspace(3, 1e5, 10)):
-    numpy_t = timeit.Timer("np.sqrt(np.inner(data, data))", 
-                           setup="import numpy as np; import ctypes;" + 
-                           "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    python_t = timeit.Timer("math.sqrt(sum([v**2 for v in data]))", 
-                           setup="import numpy as np; import ctypes; import math;" + 
-                           "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    bnh = [numpy_t.timeit(100)/python_t.timeit(100) for v in range(1)]    
-    ans[int(val)] = [np.mean(bnh), np.std(bnh)]
-    tb.progressbar(ii, 1, len(tb.logspace(3, 1e5, 10)), 'Progress')
-    
-x = np.array(ans.keys())
-x.sort()
-y = np.asarray([ans[v][0] for v in x])
-yerr = np.asarray([ans[v][1] for v in x])
-plt.errorbar(x, y, yerr, color='r', fmt='o-', label='numpy/python')
+def numpy_t(n):
+    data = np.arange(1, n , dtype=ctypes.c_double)
+    ans = np.sqrt(np.inner(data, data))
 
-#==============================================================================
-# Compare spacepy.toolbox.hypot an python
-#==============================================================================
+def extension_t(n):
+    data = np.arange(1, n , dtype=ctypes.c_double)
+    ans = tb.hypot(data)
 
-ans = {}
-for ii, val in enumerate(tb.logspace(3, 1e5, 10)):
-    extension_t = timeit.Timer("tb.hypot(data)", 
-                           setup="import numpy as np; import ctypes; import spacepy.toolbox as tb;" + 
-                           "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    python_t = timeit.Timer("math.sqrt(sum([v**2 for v in data]))", 
-                           setup="import numpy as np; import ctypes; import math;" + 
-                           "data = np.arange(1, " + str(int(val)) + ", dtype=ctypes.c_double)")
-    bnh = [extension_t.timeit(100)/python_t.timeit(100) for v in range(1)]    
-    ans[int(val)] = [np.mean(bnh), np.std(bnh)]
-    tb.progressbar(ii, 1, len(tb.logspace(3, 1e5, 10)), 'Progress')
-    
-x = np.array(ans.keys())
-x.sort()
-y = np.asarray([ans[v][0] for v in x])
-yerr = np.asarray([ans[v][1] for v in x])
-plt.errorbar(x, y, yerr, color='g', fmt='o-', label='extension/python')
-
-
-plt.xscale('log')
-plt.yscale('log')
-plt.axhline(1)
-plt.xlabel('hypot points')
-plt.ylabel('execution time ratio')
-# get the title
-rep = dulwich.repo.Repo(os.path.abspath('../../../'))
-refs = rep.get_refs()
-title = refs['HEAD']
-plt.title(title)
-plt.legend(loc='upper right', shadow=True, fancybox=True)
-plt.savefig('hypot_bench.png')
-
-
-## CONCLUSION:
-
+loop = 1000
+for bm in Benchmarker(width=25, cycle=10, extra=1):
+    bm.run(ctypes_t, loop)
+    bm.run(python_t, loop)
+    bm.run(numpy_t, loop)
+    bm.run(extension_t, loop)
 
 
 
