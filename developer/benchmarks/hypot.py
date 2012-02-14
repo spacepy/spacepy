f6bdbf9106ec3bacdf0917b6ecf0f3d8b168a0dc
Brian Larsen <balarsen@lanl.gov>
2012-02-13 17:41:16 -0700

    benchmark: final version of the hypot benchmarking in time not ratios using the new tool

--- developer/benchmarks/hypot.py
+++ developer/benchmarks/hypot.py
@@ -12,14 +12,15 @@ import spacepy.toolbox as tb
 
 # this is a git access module useful in here.
 import dulwich
+from benchmarker import Benchmarker
 
+import matplotlib.pyplot as plt
 
 lib = ctypes.CDLL('hypot.so')
 lib.hypot_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_long]
 lib.hypot.restype = ctypes.c_double
 
 
-from benchmarker import Benchmarker
 #==============================================================================
 # Compare them all
 #==============================================================================
@@ -43,13 +44,31 @@ def extension_t(n):
     data = np.arange(1, n , dtype=ctypes.c_double)
     ans = tb.hypot(data)
 
-loop = 1000
-for bm in Benchmarker(width=25, cycle=10, extra=1):
-    bm.run(ctypes_t, loop)
-    bm.run(python_t, loop)
-    bm.run(numpy_t, loop)
-    bm.run(extension_t, loop)
+ans = {}
+ans['ctypes_t'] = []
+ans['python_t'] = []
+ans['numpy_t'] = []
+ans['extension_t'] = []
 
+for loop in tb.logspace(3, 1e7, 30):
+    print "loop", loop
+    for bm in Benchmarker(width=25, cycle=5, extra=1):
+        bm.run(ctypes_t, loop)
+        bm.run(python_t, loop)
+        bm.run(numpy_t, loop)
+        bm.run(extension_t, loop)
+    for result in bm.results:
+        ans[result.label].append((loop, result.real))
 
+for key in ans:
+    plt.loglog(zip(*ans[key])[0], zip(*ans[key])[1],'o-',  label=key[:-2], lw=2 )
 
+rep = dulwich.repo.Repo(os.path.abspath('../../'))
+refs = rep.get_refs()
+title = refs['HEAD']
+plt.title(title)
+plt.xlabel('hypot points')
+plt.ylabel('Real execution time')
+plt.legend(loc='upper left', shadow=True, fancybox=True)
+plt.savefig('hypot_bench.png')
 
