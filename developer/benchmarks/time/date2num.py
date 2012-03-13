d5d4e6f09cf121133f4a1f22f02b932a618a8998
Brian Larsen <balarsen@lanl.gov>
2012-03-13 14:56:01 -0600

    benchmark: added date2num/num2date benchmark between matplotlib and our C.  Our extension is a lot faster.

--- developer/benchmarks/time/date2num.py
+++ developer/benchmarks/time/date2num.py
@@ -50,12 +50,53 @@ plt.figure()
 for key in ans:
     plt.loglog(zip(*ans[key])[0], zip(*ans[key])[1],'o-',  label=key[:-2], lw=2 )
 
-rep = dulwich.repo.Repo(os.path.abspath('../../'))
+rep = dulwich.repo.Repo(os.path.abspath('../../../'))
 refs = rep.get_refs()
 title = refs['HEAD']
 plt.title(title)
 plt.xlabel('array size')
 plt.ylabel('Real execution time')
 plt.legend(loc='upper left', shadow=True, fancybox=True)
-plt.savefig('date2num_bench.png')
+plt.savefig('date2num_bench' + title + '.png')
 
+
+#==============================================================================
+# Compare them all
+#==============================================================================
+max_size = 1e6
+date_data = np.arange(1, max_size , dtype=ctypes.c_double) + 730120.0
+
+def num2date_mpl(data):
+    ans = num2date(data)
+
+def num2date_spt(data):
+    ans = spt.num2date(data)
+
+ans = {}
+ans['num2date_mpl'] = []
+ans['num2date_spt'] = []
+
+for loop in tb.logspace(1, max_size, 20):
+    print "loop", loop
+    for bm in Benchmarker(width=25, cycle=5, extra=1):
+        data = date_data[:loop]
+        bm.run(num2date_mpl, data)
+        bm.run(num2date_spt, data)
+    for result in bm.results:
+        ans[result.label].append((loop, result.real))
+
+#==============================================================================
+# plot up the times
+#==============================================================================
+plt.figure()
+for key in ans:
+    plt.loglog(zip(*ans[key])[0], zip(*ans[key])[1],'o-',  label=key[:-2], lw=2 )
+
+rep = dulwich.repo.Repo(os.path.abspath('../../../'))
+refs = rep.get_refs()
+title = refs['HEAD']
+plt.title(title)
+plt.xlabel('array size')
+plt.ylabel('Real execution time')
+plt.legend(loc='upper left', shadow=True, fancybox=True)
+plt.savefig('num2date_bench' + title + '.png')
