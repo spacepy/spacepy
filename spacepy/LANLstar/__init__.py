cc209a6bcca716904fff41d9b74a00589e09e7d1
Steve Morley <smorley@lanl.gov>
2012-04-19 10:40:27 -0600

    lanlstar: moved some docs to __init__ so module has docs in interpreter

--- spacepy/LANLstar/__init__.py
+++ spacepy/LANLstar/__init__.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Lstar and Lmax calculation using artificial neural network (ANN) technique.
 
+Authors: Josef Koller, Yiqun Yu
+Institution: Los Alamos National Laboratory
+Contact: jkoller@lanl.gov, yiqun@lanl.gov
+
+Copyright ?2012 Los Alamos National Security, LLC.
+
+"""
 from .LANLstar import *
