005e68fae2620b380b1933e2eda645dd9aa559a3
Jonathan Niehof <jniehof@lanl.gov>
2014-01-14 10:57:46 -0700

    plot.utils.annotate_xaxis: include output in the example

--- spacepy/plot/utils.py
+++ spacepy/plot/utils.py
@@ -508,7 +508,9 @@ def annotate_xaxis(txt, ax=None):
     >>> times = [datetime.datetime(2010, 1, 1) + datetime.timedelta(hours=i)
     ...     for i in range(0, 48, 3)]
     >>> plt.plot(times, range(16))
+    [<matplotlib.lines.Line2D object at 0x0000000>]
     >>> spacepy.plot.utils.annotate_xaxis(' UT') #mark that times are UT
+    <matplotlib.text.Text object at 0x0000000>
     """
     if ax is None:
         ax = plt.gca()
