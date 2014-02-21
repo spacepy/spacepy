6d2866303625fff2dc3f30f47da66bed295e4046
Jonathan Niehof <jniehof@lanl.gov>
2014-02-21 15:45:43 -0700

    test_spacepy: the libspacepy tests are just test_lib...

--- tests/test_spacepy.py
+++ tests/test_spacepy.py
@@ -7,7 +7,7 @@ Master test suite for SpacePy
 version: V1: 28-May-2011
 
 
-Copyright 2010-2012 Los Alamos National Security, LLC.
+Copyright 2010-2014 Los Alamos National Security, LLC.
 """
 
 import sys
@@ -32,7 +32,7 @@ from test_datamodel import *
 from test_base import *
 from test_plot_utils import *
 from test_rst import *
-from test_libspacepy import *
+from test_lib import *
 # add others here as they are written
 
 if __name__ == '__main__':
