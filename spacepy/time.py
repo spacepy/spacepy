18d31866cc6ae04ad11b4e4441092e8b6d286b77
Jonathan Niehof <jniehof@lanl.gov>
2014-02-24 09:08:14 -0700

    time.TickTock.getUTC: put in some explicit floor divisions to fix for py3k

--- spacepy/time.py
+++ spacepy/time.py
@@ -1201,11 +1201,11 @@ class Ticktock(collections.MutableSequence):
                 igreg = 15+31*(10+12*1582)
                 if ja >= igreg: # after switching to Gregorian Calendar
                     jalpha = int(((ja-1867216)-0.25)/36524.25)
-                    ja = ja + 1 + jalpha -jalpha/4
+                    ja = ja + 1 + jalpha -jalpha//4
 
                 jb = ja + 1524
                 jc = int(6680.0 + ((jb - 2439870) - 122.1) / 365.25)
-                jd = 365 * jc + jc/4
+                jd = 365 * jc + jc//4
                 je = int((jb - jd)/30.6001)
                 day = jb - jd - int(30.6001 * je)
                 month = je - 1
@@ -1227,8 +1227,9 @@ class Ticktock(collections.MutableSequence):
                 # J= J+2-12*L
                 # I= 100*(N-49)+I+L
 
-                UTC[i] = datetime.datetime(year, month, day) + datetime.timedelta(hours=12) + \
-                    datetime.timedelta(seconds = p*86400)
+                UTC[i] = datetime.datetime(year, month, int(day)) + \
+                         datetime.timedelta(hours=12) + \
+                         datetime.timedelta(seconds = p*86400)
                 if UTC[i] < datetime.datetime(1582,10,15):
                     warnings.warn("WARNING: Calendar date before the switch from Julian to Gregorian\n" +
                        "Calendar 1582-Oct-15: Use Julian Calendar dates as input")
