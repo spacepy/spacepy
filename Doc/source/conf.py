c65ba7f804a134a79aba81e3afd2a7df1ce68653
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2015-07-24 13:41:10 -0400

    More sphinx warnings.

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -84,7 +84,7 @@ release = '0.1.5'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['_templates']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
