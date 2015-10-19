781d034264805038f22e67e6038b822d3122297d
Steve Morley <smorley@lanl.gov>
2015-10-19 14:08:47 -0600

    empiricals: removed old TODO, long implemented and now covered in testing

--- spacepy/empiricals.py
+++ spacepy/empiricals.py
@@ -145,7 +145,6 @@ def getPlasmaPause(ticks, model='M2002', LT='all', omnivals=None):
         prior = priorvals[model][LThr]
         A, B = parA[model][LThr], parB[model][LThr]
 
-    #TODO: allow calling with ticks as dict of Kp (will also need UT for Kpmax)
     st, en = ticks.UTC[0]-prior, ticks.UTC[-1]
     if omnivals is None:
         omdat = om.get_omni(spt.tickrange(st, en, 1.0/24.0), dbase='QDhourly')
