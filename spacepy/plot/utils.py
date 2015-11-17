7070fd5aaf8d9fcea828b77a7eab23c82c66fb8e
Brian Larsen <balarsen@lanl.gov>
2015-11-16 22:10:01 -0500

    plot.utils: doc update, of all places that shoud have plots, it's plot

--- spacepy/plot/utils.py
+++ spacepy/plot/utils.py
@@ -761,21 +761,24 @@ def collapse_vertical(combine, others=(), leave_axis=False):
 
     Examples
     ========
-    >>> import spacepy.plot.utils
-    >>> import matplotlib.pyplot as plt
-    >>> fig = plt.figure()
-    >>> #Make three stacked subplots
-    >>> ax0 = fig.add_subplot(311)
-    >>> ax1 = fig.add_subplot(312)
-    >>> ax2 = fig.add_subplot(313)
-    >>> ax0.plot([1, 2, 3], [1, 2, 1]) #just make some lines
-    [<matplotlib.lines.Line2D object at 0x0000000>]
-    >>> ax1.plot([1, 2, 3], [1, 2, 1])
-    [<matplotlib.lines.Line2D object at 0x0000000>]
-    >>> ax2.plot([1, 2, 3], [1, 2, 1])
-    [<matplotlib.lines.Line2D object at 0x0000000>]
-    >>> #Collapse space between top two plots, leave bottom one alone
-    >>> spacepy.plot.utils.collapse_vertical([ax0, ax1], [ax2])
+    .. plot::
+        :include-source:
+    
+        >>> import spacepy.plot.utils
+        >>> import matplotlib.pyplot as plt
+        >>> fig = plt.figure()
+        >>> #Make three stacked subplots
+        >>> ax0 = fig.add_subplot(311)
+        >>> ax1 = fig.add_subplot(312)
+        >>> ax2 = fig.add_subplot(313)
+        >>> ax0.plot([1, 2, 3], [1, 2, 1]) #just make some lines
+        [<matplotlib.lines.Line2D object at 0x0000000>]
+        >>> ax1.plot([1, 2, 3], [1, 2, 1])
+        [<matplotlib.lines.Line2D object at 0x0000000>]
+        >>> ax2.plot([1, 2, 3], [1, 2, 1])
+        [<matplotlib.lines.Line2D object at 0x0000000>]
+        >>> #Collapse space between top two plots, leave bottom one alone
+        >>> spacepy.plot.utils.collapse_vertical([ax0, ax1], [ax2])
     """
     combine = tuple(combine)
     others = tuple(others)
