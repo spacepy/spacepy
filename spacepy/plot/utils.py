a1273ca04e193a4f8e63b685c6c9082883d039a3
Brian Larsen <balarsen@lanl.gov>
2016-02-10 17:05:58 -0700

    plot utilsL doc update to actually make a plot

--- spacepy/plot/utils.py
+++ spacepy/plot/utils.py
@@ -516,15 +516,18 @@ def annotate_xaxis(txt, ax=None):
 
     Examples
     ========
-    >>> import spacepy.plot.utils
-    >>> import matplotlib.pyplot as plt
-    >>> import datetime
-    >>> times = [datetime.datetime(2010, 1, 1) + datetime.timedelta(hours=i)
-    ...     for i in range(0, 48, 3)]
-    >>> plt.plot(times, range(16))
-    [<matplotlib.lines.Line2D object at 0x0000000>]
-    >>> spacepy.plot.utils.annotate_xaxis(' UT') #mark that times are UT
-    <matplotlib.text.Text object at 0x0000000>
+    .. plot::
+        :include-source:
+
+        >>> import spacepy.plot.utils
+        >>> import matplotlib.pyplot as plt
+        >>> import datetime
+        >>> times = [datetime.datetime(2010, 1, 1) + datetime.timedelta(hours=i)
+        ...     for i in range(0, 48, 3)]
+        >>> plt.plot(times, range(16))
+        [<matplotlib.lines.Line2D object at 0x0000000>]
+        >>> spacepy.plot.utils.annotate_xaxis(' UT') #mark that times are UT
+        <matplotlib.text.Text object at 0x0000000>
     """
     if ax is None:
         ax = plt.gca()
