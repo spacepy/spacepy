c491111294e82e4485a8ef185d16f7dfd26f70b7
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2016-04-12 18:15:37 -0400

    Rework plot.apionly to keep clean namespace

--- spacepy/plot/apionly.py
+++ spacepy/plot/apionly.py
@@ -1,2 +1,11 @@
-from . import *
+"""
+Import spacepy.plot without changing styles.
+
+This will import :mod:`spacepy.plot` without applying the default SpacePy
+plot styles. Access all functions and classes through attributes of
+ :mod:`~spacepy.plot`; nothing further is defined here.
+"""
+
+from . import revert_style
 revert_style()
+del revert_style
