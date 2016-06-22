110c81c1181324fc270b535f0fac111b7eb1e9da
Jonathan Niehof <jniehof@lanl.gov>
2016-06-22 07:37:48 -0600

    Fix coordinates, irbempy tests on Python 2.6

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -9,9 +9,8 @@ import numpy as np
 from spacepy.time import Ticktock
 try:
     import spacepy.irbempy as ib
-    ibflag = False
 except ImportError:
-    ibflag = True
+    pass #tests will fail, but won't bring down the entire suite
 
 __all__ = ['coordsTest']
 
@@ -19,13 +18,15 @@ __all__ = ['coordsTest']
 class coordsTest(unittest.TestCase):
     def setUp(self):
         #super(tFunctionTests, self).setUp()
-        self.cvals = spc.Coords([[1,2,4],[1,2,2]], 'GEO', 'car')
+        try:
+            self.cvals = spc.Coords([[1,2,4],[1,2,2]], 'GEO', 'car')
+        except ImportError:
+            pass #tests will fail, but won't bring down the entire suite
 
     def tearDown(self):
         #super(tFunctionTests, self).tearDown()
         pass
 
-    @unittest.skipIf(ibflag, 'Warning: spacepy.irbempy could not be imported')
     def test_coords(self):
         """Coords should create and do simple conversions"""
         np.testing.assert_equal([1,1], self.cvals.x)
@@ -34,7 +35,6 @@ class coordsTest(unittest.TestCase):
         self.cvals.ticks = Ticktock(['2002-02-02T12:00:00', '2002-02-02T12:00:00'], 'ISO') # add ticktock
         newcoord = self.cvals.convert('GSM', 'sph')
 
-    @unittest.skipIf(ibflag, 'Warning: spacepy.irbempy could not be imported')
     def test_append(self):
         c2 = spc.Coords([[6,7,8],[9,10,11]], 'GEO', 'car')
         actual = self.cvals.append(c2)
