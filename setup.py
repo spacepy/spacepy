c7659d1e34443af94ef0d75a77b9d3a5b704a4b4
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2016-09-08 12:13:21 -0400

    0.1.6 release

--- setup.py
+++ setup.py
@@ -770,7 +770,7 @@ package_data = ['data/*.*', 'pybats/sample_data/*', 'data/LANLstar/*', 'data/TS0
 
 setup_kwargs = {
     'name': 'spacepy',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'description': 'SpacePy: Tools for Space Science Applications',
     'long_description': 'SpacePy: Tools for Space Science Applications',
     'author': 'SpacePy team',
