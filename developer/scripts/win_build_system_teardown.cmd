b88196e519f562275374e80920a3ec058d1bf0c2
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2019-06-19 07:12:46 -0400

    win build teardown: fix error on uninstalling Miniconda

--- developer/scripts/win_build_system_teardown.cmd
+++ developer/scripts/win_build_system_teardown.cmd
@@ -5,4 +5,4 @@
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py2_64
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py3_32
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py3_64
-%USERPROFILE%\Miniconda3\Uninstall-Miniconda3.exe /S  /D=%USERPROFILE%\Miniconda3
+start /wait "" %USERPROFILE%\Miniconda3\Uninstall-Miniconda3.exe /S /D=%USERPROFILE%\Miniconda3
