31360f87c27f7dd7604cbff5e63c7b6b8f741c10
Jon Niehof <jtniehof@gmail.com>
2019-12-03 14:03:15 -0500

    Add integration tests for toolbox
     * Starting with tests of get_url

---
+++ tests/integration_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Integration tests for SpacePy
+
+These are heavier tests than the unit tests that may e.g. temporarily
+affect the state of the machine, rather than the fully isolated unit
+tests.
+
+Copyright 2019 SpacePy contributors
+"""
+
+
+import unittest
+
+from integration_toolbox import *
+
+
+if __name__ == '__main__':
+    unittest.main()
