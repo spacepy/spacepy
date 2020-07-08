bf04e7e93ba8fa51449904280ec3bf536fbad6b3
Dan Welling <daniel.welling@uta.edu>
2020-07-08 14:24:42 -0500

    Added two more test cases for testParseFileTime.

--- tests/test_pybats.py
+++ tests/test_pybats.py
@@ -26,8 +26,7 @@ import spacepy.pybats.gitm as gitm
 
 __all__ = ['TestParseFileTime', 'TestIdlFile', 'TestRim', 'TestBats2d',
            'TestMagGrid', 'TestSatOrbit', 'TestVirtSat', 'TestImfInput',
-           'TestExtraction', 'TestProbeIdlFile']
-
+           'TestExtraction', 'TestProbeIdlFile', 'TestParsers']
 
 class TestParseFileTime(unittest.TestCase):
     '''
@@ -45,13 +44,17 @@ class TestParseFileTime(unittest.TestCase):
                           'pybats_test', 'mag_grid_ascii.out'),
              'y=0_mhd_1_t20140410000000_n00001500.out',
              'z=0_mhd_2_e20140410-000000-000_20140410-000300-000.outs',
-             'z=0_mhd_2_n00001500_00001889.outs'
+             'z=0_mhd_2_n00001500_00001889.outs',
+             'log_n000010.log',
+             'log_e20140410-000000.log'
              ]
-    iters = [None, None, 31073, 249620, None, 1500, None, [1500, 1889]]
-    times = [None, None, 870, 18000, None, None, None, None]
+    iters = [None, None, 31073, 249620, None, 1500,
+             None, [1500, 1889], 10, None]
+    times = [None, None, 870, 18000, None, None, None, None, None, None]
     dates = [dt(2013, 9, 24, 23, 26, 0), dt(2013, 9, 24, 22, 5, 0),
              None, None, None, dt(2014, 4, 10, 0, 0),
-             [dt(2014, 4, 10, 0, 0, 0), dt(2014, 4, 10, 0, 3, 0)], None]
+             [dt(2014, 4, 10, 0, 0, 0), dt(2014, 4, 10, 0, 3, 0)], None,
+             None, dt(2014, 4, 10, 0, 0, 0)]
 
     def testParse(self):
         from spacepy.pybats import parse_filename_time
