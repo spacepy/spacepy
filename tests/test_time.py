fa2a966754d887567a8dc2aa41a9f6d5676659a8
Jon Niehof <jtniehof@gmail.com>
2020-08-12 14:09:19 -0400

    test_time: more tests to/from MJD.
     * These are essentially regression tests, but have been sanity checked.

--- tests/test_time.py
+++ tests/test_time.py
@@ -709,8 +709,12 @@ class TimeClassTests(unittest.TestCase):
 
     def test_MJD(self):
         """conversions to MJD should work"""
-        t1 = t.Ticktock(['2002-01-01T01:00:00', '2002-01-02'])
-        expected = numpy.asarray([52275.04166667, 52276.])
+        t1 = t.Ticktock(['2002-01-01T01:00:00', '2002-01-02',
+                         '1858-01-01', '1582-10-4', '1582-10-15',
+                         '1961-1-1', '1971-12-31', '1972-1-1'])
+        expected = numpy.asarray([52275.04166667, 52276.,
+                                  -320., -100841., -100840.,
+                                  37300., 41316., 41317.])
         numpy.testing.assert_almost_equal(t1.MJD, expected)
 
     @unittest.expectedFailure
@@ -761,6 +765,24 @@ class TimeClassTests(unittest.TestCase):
         numpy.testing.assert_almost_equal(
             [(t1.UTC[i] - expected[i]).total_seconds() for i in range(len(t1))],
             0., decimal=5)
+        with warnings.catch_warnings(record=True) as w:
+            warnings.filterwarnings(
+                'always', 'Calendar date before the switch from Julian.*',
+                UserWarning, '^spacepy\\.time')
+            t2 = t.Ticktock([52275 + 1. / 24, 52276.,
+                             -320., -100841., -100840.,
+                             37300., 41316., 41317.], dtype='MJD')
+        self.assertEqual(1, len(w))
+        self.assertEqual(UserWarning, w[0].category)
+        expected = [(2002, 1, 1, 1), (2002, 1, 2),
+                    (1858, 1, 1), (1582, 10, 4), (1582, 10, 15),
+                    (1961, 1, 1), (1971, 12, 31), (1972, 1, 1)]
+        expected = [datetime.datetime(*e) for e in expected]
+        numpy.testing.assert_almost_equal(
+            [(t2.UTC[i] - expected[i]).total_seconds() for i in range(len(t1))],
+            0., decimal=5)
+        # If get MJD on a non-JD basis, should be able to directly compare.
+#        numpy.testing.assert_equal(expected, t2.UTC)
 
     def test_GPS(self):
         """conversions to GPS should work"""
