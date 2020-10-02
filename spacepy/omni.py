spacepy/spacepy

commit fe42d8a4e138e7a4a2cc12fabdd1d301b98d499e

omni.omnirange: get range from UTC not RDT.
 * RDT is really poor resolution.

spacepy/omni.py
@@ -374,9 +374,8 @@ def omnirange(dbase='QDhourly'):
     if dbase not in infile:
         raise NotImplementedError('')
     with h5.File(infile[dbase], mode='r') as hfile:
-        start, end = hfile['RDT'][0], hfile['RDT'][-1]
-        start = spt.Ticktock(start, 'RDT').UTC[0]
-        end = spt.Ticktock(end, 'RDT').UTC[0]
+        tt = spt.Ticktock([hfile['UTC'][0], hfile['UTC'][-1]])
+    start, end = tt.UTC
     
     return start, end
 

tests/test_omni.py
@@ -86,7 +86,7 @@ class OmniOtherTests(unittest.TestCase):
     def test_omnirange(self):
         expected = (datetime.datetime(1999, 7, 1, 14, 0), datetime.datetime(2001, 10, 11, 23, 0))
         actual = om.omnirange('Test')
-        self.assertTrue( array(expected == actual).all() )
+        numpy.testing.assert_equal(expected, actual)
         
 
 

commit 9bf06aff88b37c9a7116ffd421c3de54eae5ffb7

omni: fix handling of OMNI databases without UTC
 * Switch away from RDT in fe42d8a4e138e7a4a2cc12fabdd1d301b98d499e broke
   the OMNI2 database, which populates UTC on read.

spacepy/omni.py
@@ -374,8 +374,17 @@ def omnirange(dbase='QDhourly'):
               'Test': testfln}
     if dbase not in infile:
         raise NotImplementedError('')
+    # Possible time variables in the HDF file and their ticktock dtype
+    timeinfo = [('UTC', 'UTC'), ('Epoch', 'ISO'), ('RDT', 'RDT')]
     with h5.File(infile[dbase], mode='r') as hfile:
-        tt = spt.Ticktock([hfile['UTC'][0], hfile['UTC'][-1]])
+        for varname, dtype in timeinfo:
+            if varname in hfile:
+                tt = spt.Ticktock([hfile[varname][0], hfile[varname][-1]],
+                                  dtype=dtype)
+                break
+        else:
+            raise ValueError('Cannot find time variable in {}'
+                             .format(infile[dbase]))
     start, end = tt.UTC
     
     return start, end