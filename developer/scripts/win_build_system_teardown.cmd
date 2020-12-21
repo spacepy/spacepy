03688740219b68daf679f6d2cd3295dfb0a1eeae
Jon Niehof <jtniehof@gmail.com>
2020-12-21 12:00:42 -0500

    Update for 0.2.2 release
     * Mark version numbers, etc.
     * Update how-to-release documentation and some scripts.

--- developer/scripts/win_build_system_teardown.cmd
+++ developer/scripts/win_build_system_teardown.cmd
@@ -7,4 +7,8 @@
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py36_64
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py37_32
 %USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py37_64
+%USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py38_32
+%USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py38_64
+%USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py39_32
+%USERPROFILE%\Miniconda3\Scripts\conda env remove -y --name py39_64
 start /wait "" %USERPROFILE%\Miniconda3\Uninstall-Miniconda3.exe /S /D=%USERPROFILE%\Miniconda3
