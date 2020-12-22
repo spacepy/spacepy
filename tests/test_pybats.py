1db4ef2f80dba6a62740e156d65d7b0bf8206f9b
Jon Niehof <jtniehof@gmail.com>
2020-12-22 12:08:37 -0500

    pybats.parse_filename_time: Fix on Windows for filenames w/directories.

--- tests/test_pybats.py
+++ tests/test_pybats.py
@@ -38,11 +38,14 @@ class TestParseFileTime(unittest.TestCase):
     files = ['mag_grid_e20130924-232600.out',
              'y=0_mhd_1_e20130924-220500-054.out',
              'y=0_mhd_2_t00001430_n00031073.out',
-             'z=0_mhd_2_t00050000_n00249620.out']
+             'z=0_mhd_2_t00050000_n00249620.out',
+             os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                          'data', 'pybats_test', 'mag_grid_ascii.out'),
+    ]
     dates = [dt(2013,9,24,23,26,0), dt(2013,9,24,22, 5,0),
-             None, None]
-    times = [None, None, 870, 18000]
-    iters = [None, None, 31073, 249620]
+             None, None, None]
+    times = [None, None, 870, 18000, None]
+    iters = [None, None, 31073, 249620, None]
 
     def testParse(self):
         from spacepy.pybats import parse_filename_time
@@ -255,7 +258,7 @@ class TestMagGrid(unittest.TestCase):
     def testOpen(self):
         # Open both binary and ascii versions of same data.
         # Ensure expected values are loaded.
-        m1 = pbs.MagGridFile(os.path.join(self.pth, 'data/', 'pybats_test', 'mag_grid_ascii.out'),
+        m1 = pbs.MagGridFile(os.path.join(self.pth, 'data', 'pybats_test', 'mag_grid_ascii.out'),
                               format='ascii')
         m2 = pbs.MagGridFile(os.path.join(self.pth, 'data', 'pybats_test', 'mag_grid_binary.out'))
 
