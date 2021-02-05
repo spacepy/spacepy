45842e632260bd33ffea6ef3dea7c2d7008df0b8
Jon Niehof <jtniehof@gmail.com>
2021-02-05 15:26:35 -0500

    spacepy_testing: support for unit test helper functions.

---
+++ tests/spacepy_testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""Helper functions for SpacePy unit tests
+
+This module is importable from test scripts that are in this directory.
+"""
+
+import os.path
+
+
+testsdir = os.path.dirname(os.path.abspath(__file__))
