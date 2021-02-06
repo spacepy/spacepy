f2fc4796eda350485f225f1bd158b382257adef0
Jon Niehof <jtniehof@gmail.com>
2021-02-06 12:31:57 -0500

    test_seapy: stop suppressing a warning.

--- tests/test_seapy.py
+++ tests/test_seapy.py
@@ -32,7 +32,7 @@ class SEATestsUniform(unittest.TestCase):
         self.unidata = [self.testval]*200
         time = list(range(200))
         self.epochs = [20,40,60,80,100,120,140,160,180]
-        with warnings.catch_warnings(record=True) as w:
+        with warnings.catch_warnings():
             warnings.filterwarnings('always', 'Window size changed .*',
                                     UserWarning, '^spacepy\\.seapy$')
             self.obj = seapy.Sea(self.unidata, time, self.epochs, verbose=False)
