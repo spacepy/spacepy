664b21683c0f649eaae44ccfcb9be252f00ec1fd
Jeremy Faden <faden@cottagesystems.com>
2023-04-16 07:54:33 -0500

    Update datamodel.py

--- spacepy/datamodel.py
+++ spacepy/datamodel.py
@@ -1340,11 +1340,104 @@ def toCDF(fname, SDobject, skeleton='', flatten=False, overwrite=False,
     if former_backward is not None:
         pycdf.lib.set_backward(former_backward)
 
+def _fromHAPI_handle_bins(data, name, bins):
+    """
+    Add non-time-varying bins variable.  The HAPI server will return either
+    centers or ranges, this is required, and the centers are either read in
+    or inferred as the average of the range.  When ranges are found, this will
+    be put into the SpaceData as DELTA_PLUS_VAR and DELTA_MINUS_VAR.
+    Parameters
+    ----------
+    data : data
+        the data to add the variable
+    name : str
+        the name of the variable
+    bins : str
+        the "bins" node of the HAPI response
+    """
+    if 'centers' in bins:
+        o = bins['centers']
+        if type(o) != str:  # string is just a reference to another variable
+            centers = [float(v) for v in bins['centers']]
+        else:
+            return
+    else:  # if 'ranges' in bins:
+        ranges = bins['ranges']
+
+    if 'centers' not in locals():
+        centers = [a[0] + (a[1] - a[0]) / 2 for a in ranges]
+
+    data[name] = datamodel.dmarray(input_array=centers)
+    data[name].attrs['UNITS'] = bins['units']
+    data[name].attrs['VAR_TYPE'] = 'support_data'
+
+    if 'ranges' in locals():
+        import numpy
+        delta_plus_var = numpy.zeros(len(centers))
+        for i in range(len(centers)):
+            delta_plus_var[i] = ranges[i][1] - centers[i]
+        delta_minus_var = numpy.zeros(len(centers))
+        for i in range(len(centers)):
+            delta_minus_var[i] = centers[i] - ranges[i][0]
+        data[name + 'DeltaMinus'] = delta_minus_var
+        data[name + 'DeltaPlus'] = delta_plus_var
+        data[name].attrs['DELTA_PLUS_VAR'] = name + 'DeltaPlus'
+        data[name].attrs['DELTA_MINUS_VAR'] = name + 'DeltaMinus'
 
 def fromHAPI(hapidata):
-    """ """
-    sd= SpaceData()
-    return sd
+    """Reformat the response from the Python hapiclient to an object similar to a cdf.  The cdf
+     will be similar to the object returned by reading a data.
+    This is typically called using the  of the Python hapiclient.
+    which is a tuple containing the data and the metadata from a HAPI
+    call.  For example:
+    hapidata = hapiclient.hapi(server, dataset, parameters, start, stop, **opts)
+    data = to_Pydata( hapidata )
+    Parameters
+    ----------
+    hapidata : tuple
+        this is a two-element tuple containing the data and metadata returned by the HAPI server via the Python hapiclient.
+    """
+
+    data, meta = hapidata
+
+    names = [m['name'] for m in meta['parameters']]
+
+    result = datamodel.SpaceData()
+
+    for i in range(len(names)):
+        name = names[i]
+        m = meta['parameters'][i]
+        if i == 0:
+            d = hapitime2datetime(data[m['name']])
+            result[name] = datamodel.dmarray(d)
+            result[name].attrs['VAR_TYPE'] = 'support_data'
+        else:
+            d = data[name]
+            result[name] = datamodel.dmarray(d)
+            v = result[name]
+            if 'bins' in m:
+                bins = m['bins']
+                idep = 1
+                if isinstance(bins, dict):
+                    refstr = bins.get('$ref')
+                    ref = re.match('\#\/definitions\/(.+)', refstr).group(1)
+                    bins = meta['definitions'][ref]
+
+                for b in bins:
+                    # TODO: figure out why this works when multiple variables use the same bins.
+                    _fromHAPI_handle_bins(result, b['name'], b)
+                    v.attrs['DEPEND_%d' % idep] = b['name']
+                    idep = idep + 1
+            v.attrs['UNITS'] = ' ' if m['units'] is None else m['units']
+            v.attrs['DEPEND_0'] = meta['parameters'][0]['name']
+            v.attrs['VAR_TYPE'] = 'data'
+
+        if 'description' in m:
+            result[name].attrs['CATDESC'] = m['description']
+
+    result.attrs = {'CreateDate': datetime.datetime.now()}
+
+    return result
 
 
 def fromHDF5(fname, **kwargs):
