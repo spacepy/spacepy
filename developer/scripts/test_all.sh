edee1891872fe94275f119544f6d3be7f9703442
Jon Niehof <jtniehof@gmail.com>
2023-07-07 11:45:01 -0400

    Explicitly define versions of wheel to support

--- developer/scripts/test_all.sh
+++ developer/scripts/test_all.sh
@@ -17,16 +17,16 @@ source ${HOME}/cdf/bin/definitions.B
 # coupled with what's the earliest and latest version of each dep
 # which works with that Python.
 TESTS=(
-       "3.6|pip~=20.0.0 setuptools~=44.1.0|numpy~=1.15.1|python-dateutil~=2.1.0 scipy~=1.0.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=1.0,<1.1|old"
-       "3.6|pip setuptools|numpy>=1.19.0,<1.20.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.7|pip~=20.0.0 setuptools~=44.1.0|numpy>=1.15.1,<1.16.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.7|pip setuptools|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.8|pip~=20.0.0 setuptools~=44.1.0|numpy>=1.17.0,<1.18.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.8|pip setuptools|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.9|pip~=20.0.0 setuptools~=44.1.0|numpy>=1.18.0,<1.19.0|python-dateutil~=2.1.0 scipy>=1.5.0,<1.6.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.9|pip setuptools|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.10|pip~=20.0.0 setuptools~=44.1.0|numpy>=1.21.0,<1.22.0|python-dateutil~=2.7.0 scipy>=1.7.2,<1.8.0 matplotlib~=3.1.0 h5py>=3.6,<3.7 astropy>=4.0,<4.1|old"
-       "3.10|pip setuptools|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.6|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy~=1.15.1|python-dateutil~=2.1.0 scipy~=1.0.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=1.0,<1.1|old"
+       "3.6|pip setuptools wheel|numpy>=1.19.0,<1.20.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.7|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.15.1,<1.16.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.7|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.8|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.17.0,<1.18.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.8|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.9|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.18.0,<1.19.0|python-dateutil~=2.1.0 scipy>=1.5.0,<1.6.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.9|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.10|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.21.0,<1.22.0|python-dateutil~=2.7.0 scipy>=1.7.2,<1.8.0 matplotlib~=3.1.0 h5py>=3.6,<3.7 astropy>=4.0,<4.1|old"
+       "3.10|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
       )
 for thisTest in "${TESTS[@]}"
 do
