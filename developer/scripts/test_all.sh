ae73859c31d83853f60fb339b4a91e9c318d5f6b
Jon Niehof <jtniehof@gmail.com>
2023-08-01 14:07:33 -0400

    ci: explicitly handle cython dependency for numpy
     * fixes a CI failure for numpy 1.18 on python 3.9, which doesn't work
       with newest cython (3.0.0)

--- developer/scripts/test_all.sh
+++ developer/scripts/test_all.sh
@@ -17,16 +17,16 @@ source ${HOME}/cdf/bin/definitions.B
 # coupled with what's the earliest and latest version of each dep
 # which works with that Python.
 TESTS=(
-       "3.6|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy~=1.15.1|python-dateutil~=2.1.0 scipy~=1.0.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=1.0,<1.1|old"
-       "3.6|pip setuptools wheel|numpy>=1.19.0,<1.20.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.7|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.15.1,<1.16.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.7|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.8|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.17.0,<1.18.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.8|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.9|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.18.0,<1.19.0|python-dateutil~=2.1.0 scipy>=1.5.0,<1.6.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
-       "3.9|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.10|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2|numpy>=1.21.0,<1.22.0|python-dateutil~=2.7.0 scipy>=1.7.2,<1.8.0 matplotlib~=3.1.0 h5py>=3.6,<3.7 astropy>=4.0,<4.1|old"
-       "3.10|pip setuptools wheel|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.6|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython|numpy~=1.15.1|python-dateutil~=2.1.0 scipy~=1.0.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=1.0,<1.1|old"
+       "3.6|pip setuptools wheel cython|numpy>=1.19.0,<1.20.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.7|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython|numpy>=1.15.1,<1.16.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.7|pip setuptools wheel cython|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.8|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython|numpy>=1.17.0,<1.18.0|python-dateutil~=2.1.0 scipy>=1.0.0,<1.1.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.8|pip setuptools wheel cython|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.9|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython<3.0|numpy>=1.18.0,<1.19.0|python-dateutil~=2.1.0 scipy>=1.5.0,<1.6.0 matplotlib~=3.1.0 h5py~=2.10.0 astropy>=2.0,<2.1|old"
+       "3.9|pip setuptools wheel|numpy>=1.21.0 cython|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.10|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython|numpy>=1.21.0,<1.22.0|python-dateutil~=2.7.0 scipy>=1.7.2,<1.8.0 matplotlib~=3.1.0 h5py>=3.6,<3.7 astropy>=4.0,<4.1|old"
+       "3.10|pip setuptools wheel cython|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
       )
 for thisTest in "${TESTS[@]}"
 do
@@ -39,13 +39,12 @@ do
     ENVNAME=py${PYVER//.}
     ~/miniconda/bin/conda create -y -n ${ENVNAME} python=${PYVER}
     source ~/miniconda/bin/activate ${ENVNAME}
-    # Get the preferred version of pip and setuptools
+    # Get the preferred version of install tools, numpy's dependencies
     pip install --no-build-isolation ${PIP}
     # Get numpy in first (lots uses distutils)
-    # ALLOW build isolation here so it can grab cython if necessary
-    pip install ${NUMPY}
-    # But don't let it grab a different version of numpy later,
-    # and build against the installed numpy
+    pip install --no-build-isolation ${NUMPY}
+    # Don't let it grab a different version of numpy later,
+    # and always build against the installed numpy
     if [ "$DEPSTRAT" = "old" -a \( ${PYVER} = "3.6" -o ${PYVER} = "3.7" -o ${PYVER} = "3.8" \) ]; then
 	# scipy < 1.5 doesn't build on gcc 10+ without this argument
 	FOPT="-fallow-argument-mismatch" pip install --no-build-isolation ${NUMPY} ${PIPLIST}
