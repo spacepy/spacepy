spacepy/spacepy

commit 013619670dcf3d5ea1cbad994349da57105f6273

ci: include FOPT for old scipy in dependency build

.github/workflows/ci.yml
@@ -151,6 +151,8 @@ jobs:
         PIPLIST: ${{ matrix.piplist }}
         CFLAGS: ${{ matrix.cflags }}
         CDF_VER: ${{ matrix.cdf-munged-version }}
+        PYVER: ${{ matrix.python-version }}
+        DEPSTRAT: ${{ matrix.dep-strategy }}
       run: |
         sudo apt-get update -qq
         # Needed for scipy versions without binary wheels
@@ -161,7 +163,12 @@ jobs:
         # ALLOW build isolation here so it can grab cython if necessary
         pip install --force-reinstall "numpy${NUMPY_VERSION}"
         # Make sure new packages don't override numpy version
-        pip install "numpy${NUMPY_VERSION}" ${PIPLIST}
+        if [ "$DEPSTRAT" = "old" -a \( ${PYVER} = "3.6" -o ${PYVER} = "3.7" -o ${PYVER} = "3.8" \) ]; then
+            # scipy < 1.5 doesn't build on gcc 10+ without this argument
+            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation --log=install_log.txt ${NUMPY} ${PIPLIST}
+        else
+            pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+        fi
         pip freeze --all
         if [ ! -d ${HOME}/cdf ]; then wget https://spdf.gsfc.nasa.gov/pub/software/cdf/dist/cdf${CDF_VER}/linux/cdf${CDF_VER}-dist-cdf.tar.gz; tar xzf cdf${CDF_VER}-dist-cdf.tar.gz; cd cdf${CDF_VER}-dist; make OS=linux ENV=gnu SHARED=yes CURSES=no FORTRAN=no all; make INSTALLDIR=$HOME/cdf/ install.lib install.definitions; rm -f ${HOME}/cdf/lib/libcdf.a; cd ..; fi
 # Per https://github.com/actions/checkout/issues/15, this gets the MERGE
@@ -250,6 +257,8 @@ jobs:
         PIPLIST: ${{ matrix.piplist }}
         CFLAGS: ${{ matrix.cflags }}
         CDF_VER: ${{ matrix.cdf-munged-version }}
+        PYVER: ${{ matrix.python-version }}
+        DEPSTRAT: ${{ matrix.dep-strategy }}
       run: |
         sudo apt-get update -qq
         # Needed for scipy versions without binary wheels
@@ -257,9 +266,15 @@ jobs:
         python -m pip install --no-build-isolation ${BUILD_DEPS}
         # This allows pip to build (and thus cache) binary wheels
         pip install wheel
+        # ALLOW build isolation here so it can grab cython if necessary
         pip install --force-reinstall "numpy${NUMPY_VERSION}"
         # Make sure new packages don't override numpy version
-        pip install "numpy${NUMPY_VERSION}" ${PIPLIST}
+        if [ "$DEPSTRAT" = "old" -a \( ${PYVER} = "3.6" -o ${PYVER} = "3.7" -o ${PYVER} = "3.8" \) ]; then
+            # scipy < 1.5 doesn't build on gcc 10+ without this argument
+            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+        else
+            pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+        fi
         pip freeze --all
         if [ ! -d ${HOME}/cdf ]; then wget https://spdf.gsfc.nasa.gov/pub/software/cdf/dist/cdf${CDF_VER}/linux/cdf${CDF_VER}-dist-cdf.tar.gz; tar xzf cdf${CDF_VER}-dist-cdf.tar.gz; cd cdf${CDF_VER}-dist; make OS=linux ENV=gnu SHARED=yes CURSES=no FORTRAN=no all; make INSTALLDIR=$HOME/cdf/ install.lib install.definitions; rm -f ${HOME}/cdf/lib/libcdf.a; cd ..; fi
 # Per https://github.com/actions/checkout/issues/15, this gets the MERGE

commit 639908b470a5a4e4fb08e851818e2e6a4c14706a

Update CI, build system, etc. for Python 3.12
 * astropy, scipy, h5py bumped for binaries; might work with earlier versions.
 * f2py breaks for numpy 1.26+ on Python 3.12, so limit to 1.25
 * Fix "pinning" of numpy version in CI
   * broken in 013619670dcf3d5ea1cbad994349da57105f6273

.github/workflows/ci.yml
@@ -137,6 +137,24 @@ jobs:
             cdf-version: "3.8.1"
             cdf-munged-version: "38_1"
             dep-strategy: "newest"
+          - python-version: "3.12"
+            os: ubuntu-22.04
+            build-deps: "pip~=23.1.0 setuptools~=66.1.0 wheel~=0.34.2 cython<3.0"
+            numpy-version: "~=1.24.0"
+            piplist: "python-dateutil~=2.7.0 scipy~=1.11.0 matplotlib~=3.5.0 h5py~=3.10.0 astropy~=5.3.0"
+            cflags: "-Wno-error=format-security"
+            cdf-version: "3.5.1"
+            cdf-munged-version: "35_1"
+            dep-strategy: "oldest"
+          - python-version: "3.12"
+            os: ubuntu-22.04
+            build-deps: "pip setuptools wheel cython"
+            numpy-version: "<1.26.0"
+            piplist: "python-dateutil scipy matplotlib h5py astropy"
+            cflags: ""
+            cdf-version: "3.8.1"
+            cdf-munged-version: "38_1"
+            dep-strategy: "newest"
     steps:
     - name: Set up Python ${{ matrix.python-version }}
       uses: actions/setup-python@v4
@@ -180,9 +198,9 @@ jobs:
         # Make sure new packages don't override numpy version
         if [ "$DEPSTRAT" = "oldest" -a \( ${PYVER} = "3.6" -o ${PYVER} = "3.7" -o ${PYVER} = "3.8" \) ]; then
             # scipy < 1.5 doesn't build on gcc 10+ without this argument
-            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation --log=install_log.txt ${NUMPY} ${PIPLIST}
+            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation --log=install_log.txt numpy${NUMPY_VERSION} ${PIPLIST}
         else
-            pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+            pip install --no-build-isolation numpy${NUMPY_VERSION} ${PIPLIST}
         fi
         pip freeze --all
         if [ ! -d ${HOME}/cdf ]; then wget https://spdf.gsfc.nasa.gov/pub/software/cdf/dist/cdf${CDF_VER}/linux/cdf${CDF_VER}-dist-cdf.tar.gz; tar xzf cdf${CDF_VER}-dist-cdf.tar.gz; cd cdf${CDF_VER}-dist; make OS=linux ENV=gnu SHARED=yes CURSES=no FORTRAN=no all; make INSTALLDIR=$HOME/cdf/ install.lib install.definitions; rm -f ${HOME}/cdf/lib/libcdf.a; cd ..; fi
@@ -228,10 +246,10 @@ jobs:
             cdf-version: "3.5.1"
             cdf-munged-version: "35_1"
             dep-strategy: "oldest"
-          - python-version: "3.11"
+          - python-version: "3.12"
             os: ubuntu-22.04
             build-deps: "pip setuptools wheel cython"
-            numpy-version: ">=1.26.0"
+            numpy-version: "<1.26.0"
             piplist: "python-dateutil scipy matplotlib h5py astropy sphinx numpydoc"
             cflags: ""
             cdf-version: "3.8.1"
@@ -283,9 +301,9 @@ jobs:
         # Make sure new packages don't override numpy version
         if [ "$DEPSTRAT" = "oldest" -a \( ${PYVER} = "3.6" -o ${PYVER} = "3.7" -o ${PYVER} = "3.8" \) ]; then
             # scipy < 1.5 doesn't build on gcc 10+ without this argument
-            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+            FOPT="-fallow-argument-mismatch" pip install --no-build-isolation numpy${NUMPY_VERSION} ${PIPLIST}
         else
-            pip install --no-build-isolation ${NUMPY} ${PIPLIST}
+            pip install --no-build-isolation numpy${NUMPY_VERSION} ${PIPLIST}
         fi
         pip freeze --all
         if [ ! -d ${HOME}/cdf ]; then wget https://spdf.gsfc.nasa.gov/pub/software/cdf/dist/cdf${CDF_VER}/linux/cdf${CDF_VER}-dist-cdf.tar.gz; tar xzf cdf${CDF_VER}-dist-cdf.tar.gz; cd cdf${CDF_VER}-dist; make OS=linux ENV=gnu SHARED=yes CURSES=no FORTRAN=no all; make INSTALLDIR=$HOME/cdf/ install.lib install.definitions; rm -f ${HOME}/cdf/lib/libcdf.a; cd ..; fi

developer/scripts/build_win.cmd
@@ -7,7 +7,7 @@ SETLOCAL EnableDelayedExpansion
 set PYTHONPATH=
 set PATH=
 
-FOR %%B in (64) DO (FOR %%P in (36 37 38 39 310 311) DO CALL :build %%B %%P)
+FOR %%B in (64) DO (FOR %%P in (36 37 38 39 310 311 312) DO CALL :build %%B %%P)
 
 GOTO :EOF
 

developer/scripts/win_build_system_setup.cmd
@@ -17,6 +17,7 @@ start /wait "" "%USERPROFILE%\Downloads\Miniconda3-latest-Windows-x86_64.exe" /I
 :: base environment needs to be activated
 CALL "%SYSTEMDRIVE%\Miniconda3\Scripts\activate"
 CALL conda update -y conda
+CALL conda create -y -n py312_64 python=3.12
 CALL conda create -y -n py311_64 python=3.11
 CALL conda create -y -n py310_64 python=3.10
 CALL conda create -y -n py39_64 python=3.9
@@ -47,6 +48,9 @@ IF "%ACTION%"=="BUILD" (
     CALL conda install -y m2w64-gcc-fortran libpython
     set NUMPY="numpy"
     :: Build with the minimum version for each Python version
+    IF "%2"=="312" (
+        set NUMPY="numpy~=1.24.0"
+    )
     IF "%2"=="311" (
         set NUMPY="numpy~=1.22.0"
     )

developer/scripts/win_build_system_teardown.cmd
@@ -7,4 +7,5 @@
 "%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py39_64
 "%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py310_64
 "%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py311_64
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py312_64
 start /wait "" "%SYSTEMDRIVE%\Miniconda3\Uninstall-Miniconda3.exe" /S /D=%SYSTEMDRIVE%\Miniconda3

pyproject.toml
@@ -8,7 +8,8 @@ requires = ["setuptools", "wheel",
     "numpy ~= 1.18.0 ; python_version == '3.9' and (platform_system != 'Darwin' or platform_machine != 'arm64')",
     "numpy ~= 1.21.0 ; python_version == '3.9' and platform_system == 'Darwin' and platform_machine == 'arm64'",
     "numpy ~= 1.21.0 ; python_version == '3.10'",
-    "numpy ~= 1.22.0 ; python_version >= '3.11'",
+    "numpy ~= 1.22.0 ; python_version == '3.11'",
+    "numpy ~= 1.24.0 ; python_version >= '3.12'",
     ]
 build-backend = "setuptools.build_meta"
 

developer/scripts/test_all.sh
@@ -27,8 +27,10 @@ TESTS=(
        "3.9|pip setuptools wheel|numpy>=1.21.0 cython|python-dateutil scipy matplotlib h5py astropy|new"
        "3.10|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython|numpy>=1.21.0,<1.22.0|python-dateutil~=2.7.0 scipy>=1.7.2,<1.8.0 matplotlib~=3.1.0 h5py>=3.6,<3.7 astropy>=4.0,<4.1|old"
        "3.10|pip setuptools wheel cython|numpy>=1.21.0|python-dateutil scipy matplotlib h5py astropy|new"
-       "3.11|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython<3.0|numpy~=1.22.0|python-dateutil~=2.7.0 scipy==1.9.0 matplotlib~=3.1.0 h5py~=3.6.0 astropy~=5.0.0|old"
+       "3.11|pip~=20.0.0 setuptools~=44.1.0 wheel~=0.34.2 cython<3.0|numpy~=1.22.0|python-dateutil~=2.7.0 scipy~=1.9.0 matplotlib~=3.1.0 h5py~=3.8.0 astropy~=5.0.0|old"
        "3.11|pip setuptools wheel cython|numpy>=1.26.0|python-dateutil scipy matplotlib h5py astropy|new"
+       "3.12|pip~=23.1.0 setuptools~=66.1.0 wheel~=0.34.2 cython<3.0|numpy~=1.24.0|python-dateutil~=2.7.0 scipy~=1.11.0 matplotlib~=3.5.0 h5py~=3.10.0 astropy~=5.3.0|old"
+       "3.12|pip setuptools wheel cython|numpy<1.26.0|python-dateutil scipy matplotlib h5py astropy|new"
       )
 for thisTest in "${TESTS[@]}"
 do