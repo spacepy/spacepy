78a897a7f042afb96241204769092a1cafa2c06a
Jon Niehof <jtniehof@gmail.com>
2023-12-15 18:57:00 -0500

    Fix windows installer on 3.6; remove last 32-bit references
     * build is only available via pip for 3.6, not conda
     * Remove scattered "64", since it's all 64 now

--- developer/scripts/win_build_system_teardown.cmd
+++ developer/scripts/win_build_system_teardown.cmd
@@ -1,11 +1,11 @@
 :: Remove the Windows build system
 @ECHO OFF
 
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py36_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py37_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py38_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py39_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py310_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py311_64
-"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py312_64
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py36
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py37
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py38
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py39
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py310
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py311
+"%SYSTEMDRIVE%\Miniconda3\Scripts\conda" env remove -y --name py312
 start /wait "" "%SYSTEMDRIVE%\Miniconda3\Uninstall-Miniconda3.exe" /S /D=%SYSTEMDRIVE%\Miniconda3
