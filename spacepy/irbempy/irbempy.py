2e6bb006fd06f1da0fa58985c6dcc0fb5cab9db7
James <jamesmw732@gmail.com>
2024-06-11 11:55:59 -0400

    irbempy: convert make_lstar1 to ctypes

--- spacepy/irbempy/irbempy.py
+++ spacepy/irbempy/irbempy.py
@@ -1884,17 +1884,10 @@ def _get_Lstar(ticks, loci, alpha, extMag='T01STORM', options=[1, 0, 0, 0, 0],
             d['sysaxes'], d['iyearsat'], d['idoysat'], d['utsat'],
             d['xin1'], d['xin2'], d['xin3'], d['magin']]
     if no_shell_splitting:  # no drift shell splitting
-        func = irbemlib.landi2lstar1 if landi2lstar else oplib.make_lstar1
-    else:  # with drift shell splitting
-        # Drift shell splitting requires pitch angle positional args
-        args.insert(1, nalpha)
-        args.insert(-1, d['degalpha'])
-        func = oplib.landi2lstar_shell_splitting1 if landi2lstar \
-               else oplib.make_lstar_shell_splitting1
-
-    if func is irbemlib.landi2lstar1:
+        func = irbemlib.landi2lstar1 if landi2lstar else irbemlib.make_lstar1
         NTIME_MAX = 100000
         NENE_MAX = 25
+        d['magin'] = np.require(d['magin'], requirements='F')
         lm = np.empty((NTIME_MAX,), np.float64)
         lstar = np.empty((NTIME_MAX,), np.float64)
         bmirr = np.empty((NTIME_MAX,), np.float64)
@@ -1903,7 +1896,7 @@ def _get_Lstar(ticks, loci, alpha, extMag='T01STORM', options=[1, 0, 0, 0, 0],
         mlt = np.empty((NTIME_MAX,), np.float64)
         func(int4(nTAI), int4(d['kext']), (int4 * 5)(*options), int4(d['sysaxes']),
             d['iyearsat'].ctypes.data_as(ctypes.POINTER(int4 * NTIME_MAX)),
-            d['idoysat'].ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX)),
+            d['idoysat'].ctypes.data_as(ctypes.POINTER(int4 * NTIME_MAX)),
             d['utsat'].ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX)),
             d['xin1'].ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX)),
             d['xin2'].ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX)),
@@ -1916,7 +1909,12 @@ def _get_Lstar(ticks, loci, alpha, extMag='T01STORM', options=[1, 0, 0, 0, 0],
             xj.ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX)),
             mlt.ctypes.data_as(ctypes.POINTER(real8 * NTIME_MAX))
         )
-    else:
+    else:  # with drift shell splitting
+        # Drift shell splitting requires pitch angle positional args
+        args.insert(1, nalpha)
+        args.insert(-1, d['degalpha'])
+        func = oplib.landi2lstar_shell_splitting1 if landi2lstar \
+               else oplib.make_lstar_shell_splitting1
         # For a locally 90-degree particle, bmirr is blocal
         lm, lstar, bmirr, bmin, xj, mlt = func(*args)
 
@@ -2412,6 +2410,11 @@ def _load_lib():
                        real8 * JMAXI, (real8 * 3) * IMAXI, (real8 * 3) * IMAXI,
                        (real8 * 3) * IMAXI, (real8 * 3) * IMAXI, (real8 * 3) * IMAXI),
         'landi2lstar1': (int4, int4, int4 * 5, int4, int4 * NTIME_MAX, int4 * NTIME_MAX,
+                         real8 * NTIME_MAX, real8 * NTIME_MAX, real8 * NTIME_MAX,
+                         real8 * NTIME_MAX, (real8 * NENE_MAX) * NTIME_MAX,
+                         real8 * NTIME_MAX, real8 * NTIME_MAX, real8 * NTIME_MAX,
+                         real8 * NTIME_MAX, real8 * NTIME_MAX, real8 * NTIME_MAX),
+        'make_lstar1': (int4, int4, int4 * 5, int4, int4 * NTIME_MAX, int4 * NTIME_MAX,
                          real8 * NTIME_MAX, real8 * NTIME_MAX, real8 * NTIME_MAX,
                          real8 * NTIME_MAX, (real8 * NENE_MAX) * NTIME_MAX,
                          real8 * NTIME_MAX, real8 * NTIME_MAX, real8 * NTIME_MAX,
